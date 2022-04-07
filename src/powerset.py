e3483f995dd8c7c8488f4532ff44ba145ce27212
Thomas Mailund <mailund@birc.au.dk>
2022-04-07 13:24:12 +0200

    doctest sucks here

--- src/powerset.py
+++ src/powerset.py
@@ -8,12 +8,5 @@ def power(x: list[Any]) -> list[list[Any]]:
     Compute the power-set of x.
 
     Returns the power-set of x as a list of lists.
-
-    >>> power([])
-    [[]]
-    >>> power([1])
-    [[], [1]]
-    >>> power([1, 2])
-    [[], [1], [2], [1, 2]]
     """
     return []  # FIXME: you need to add some code here.
